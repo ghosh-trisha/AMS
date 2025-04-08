a546eefe1fa1c2aa8c274d986360fdd422e887ad
ghosh-trisha <trisha.ghosh1620@gmail.com>
2025-04-08 08:51:46 +0530

    commited

--- server/models/Schedule.js
+++ server/models/Schedule.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
 const ScheduleTeacherMapper = require('./ScheduleTeacherMapper');
 
-const scheduleSchema = new mongoose.Schema({ 
-    sessionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Session', required: true },
-    day: { type: String, enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], required: true },
-    start_time: { type: String, required: true },
-    end_time: { type: String, required: true },
-    subjectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
+const scheduleSchema = new mongoose.Schema({
+  sessionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Session', required: true },
+  day: { type: String, enum: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"], required: true },
+  start_time: { type: String, required: true },
+  end_time: { type: String, required: true },
+  subjectId: { type: mongoose.Schema.Types.ObjectId, ref: 'Subject', required: true },
 }, {
-    timestamps: true
+  timestamps: true
 });
+
+
 scheduleSchema.pre('findOneAndDelete', async function (next) {
-    const doc = await this.model.findOne(this.getFilter());
-    if (doc) {
-      await ScheduleTeacherMapper.deleteMany({ scheduleId: doc._id });
-    }
-    next();
-  });
+  const doc = await this.model.findOne(this.getFilter());
+  if (doc) {
+    await ScheduleTeacherMapper.deleteMany({ scheduleId: doc._id });
+  }
+  next();
+});
 
 module.exports = mongoose.model('Schedule', scheduleSchema);
--- server/models/Teacher.js
+++ server/models/Teacher.js
@@ -1,29 +1,38 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const ScheduleTeacherMapper = require('./ScheduleTeacherMapper'); 
 
-const teacherSchema = new mongoose.Schema({ 
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    phone: { type: String, required: true },
-    password: {
-        type: String,
-        required: true,
-      }
+const teacherSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  password: {
+    type: String,
+    required: true,
+  }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 // Mongoose Hook to Hash Password Before Saving
 teacherSchema.pre('save', async function (next) {
-    if (this.isModified('password')) {
-        this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-  });
-  
-  // Custom Method to Compare Password
-  teacherSchema.methods.isPasswordCorrect = async function (password) {
-    return await bcrypt.compare(password, this.password);
-  };
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Custom Method to Compare Password
+teacherSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+teacherSchema.pre('findOneAndDelete', async function (next) {
+  const doc = await this.model.findOne(this.getFilter());
+  if (doc) {
+    await ScheduleTeacherMapper.deleteMany({ teacherId: doc._id });
+  }
+  next();
+});
 
 module.exports = mongoose.model('Teacher', teacherSchema);
